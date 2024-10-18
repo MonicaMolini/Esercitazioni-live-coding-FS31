b5b9bf7d25f5f0371f9b9d447accdf89950fa3b1
Monica Molini <122270090+MonicaMolini@users.noreply.github.com>
2024-10-18 12:56:31 +0200

    lezione 7 Javascript + esercizi per weekend

---
+++ esercizi.js
@@ -0,0 +1,23 @@
+//NB: Questi esercizi sono strutturati per farti apprendere come concatenare più metodi in una volta: cerca di scrivere meno codice possibile
+
+//Esercizio 1
+//Dato il seguente array di numeri, filtra i numeri pari e calcola la loro somma.
+const numberi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//Esercizio 2
+//Dato il seguente array di parole, trasforma tutte le parole in maiuscolo e poi conta quante lettere ci sono in totale.
+const parole = ["ciao", "albero", "parco", "mese", "divertimento"];
+
+//Esercizio 3
+// Dato il seguente array di numeri che contiente sia valori positivi che negativi, filtra i numeri positivi e calcola la loro media.
+const numberiMix = [-5, 10, 15, -20, 25, 30];
+
+//Esercizio 4
+//Dato il seguente array di frutti, crea un oggetto che conta le occorrenze di ciascun frutto e poi filtra l'oggetto per mostrare solo i frutti che compaiono più di una volta.
+const frutti = ["apple", "banana", "apple", "orange", "banana", "banana"];
+
+//Esercizio 5
+//Crea un array di oggetti rappresentanti studenti (con nome e voto), filtra gli studenti che hanno un voto maggiore o uguale a 60 e crea un nuovo array di oggetti contenente solo i nomi degli studenti e il loro voto.
+
+//Esercizio 6
+//Prendi come riferimento l'array di numeri dell'esercizio 1. Filtra i numeri pari, calcola il quadrato di ciascuno e poi somma tutti i quadrati. (Suggerimento: l'operazione per calcolare il quadrato è numero ** 2)
